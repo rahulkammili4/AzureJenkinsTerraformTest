c0f21c9d4db11fee05aea0a7c1e1e14481fc9e8d
Rahul Kammili (DHL IT Services) <rahul.kammili@dhl.com>
2022-09-22 13:47:53 +0530

    V1

--- providers.tf
+++ providers.tf
@@ -19,3 +19,4 @@ provider "azurerm" {
   client_id       = "47bd0b23-048e-4c2a-8596-7c0a173ed16c"
   client_secret   = "AIk8Q~ep2-w6b_lULGAEALRtYxlxAcB.gsf.zaYQ"
   tenant_id       = "65915fe2-b321-45d3-968a-6baafd475180"
+}
