df6f2f69f0630ce6eca9bd576b88b0e677a87b9c
Rahul Kammili (DHL IT Services) <rahul.kammili@dhl.com>
2022-09-22 13:41:48 +0530

    SP

--- providers.tf
+++ providers.tf
@@ -15,4 +15,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
+  subscription_id = "fa1e045c-67f6-48e0-83e1-030d1ca1977f"
+  client_id       = "47bd0b23-048e-4c2a-8596-7c0a173ed16c"
+  client_secret   = "AIk8Q~ep2-w6b_lULGAEALRtYxlxAcB.gsf.zaYQ"
+  tenant_id       = "65915fe2-b321-45d3-968a-6baafd475180"
